1eec3520055be9637c72b2ae89f25b0c28715a4d
chisl <info@chisl.io>
2017-12-27 18:13:00 +0100

    Initial commit

---
+++ HMC5883L.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        HMC5883L
+ * description: Surface-mount, multi-chip module designed for low-field magnetic sensing with a digital interface for applications such as lowcost compassing and magnetometry
+ * manuf:       Honeywell
+ * version:     0.1
+ * url:         https://aerocontent.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/HMC5883L_3-Axis_Digital_Compass_IC.pdf
+ * date:        2016-08-01
+ * author       https://chisl.io/
+ * file:        HMC5883L.cpp
+ */
+
+#include "HMC5883L.hpp"
+
